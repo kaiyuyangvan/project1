524388e734ff87a93fa63129212682db9def364c
kaiyuyangvan <41171873+kaiyuyangvan@users.noreply.github.com>
2018-07-29 10:40:02 -0700

    update

--- verifyBackupDB.sh
+++ verifyBackupDB.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
-	echo "Error: no augument provided "
-        echo "usage: $0 backup_sql_file"
+if [ $# -lt 2 ]; then
+	echo "Error: Two augument need to be provided "
+        echo "usage: $0 backup_sql_file root_passwd"
         exit 1
 fi
 
 echo "Will restore backup database from:" $1
-mysql -uroot -proot -e "show databases" | grep -v Database | grep -v mysql | grep -v information_schema | gawk '{print "drop database " $1 ";select sleep(0.1);" }' | mysql -uroot -proot
+mysql -uroot -p$2 -e "show databases" | grep -v Database | grep -v mysql | grep -v information_schema | gawk '{print "drop database " $1 ";select sleep(0.1);" }' | mysql -uroot -p$2
 echo "Clean all databases before restore"
-mysql -uroot -proot -e "show databases"
+mysql -uroot -p$2 -e "show databases"
 echo "Restore Backupfile from $1 "
-mysql -uroot -proot < $1
+mysql -uroot -p$2 < $1
 echo " All databases after restore"
-mysql -uroot -proot -e "show databases"
+mysql -uroot -p$2 -e "show databases"
